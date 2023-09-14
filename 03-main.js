4961047879e4c8ada784631551e010534d39de22
Boza Patrik <tempestsama002@gmail.com>
2023-09-14 20:06:55 +0200

    02-Módosított

---
+++ 03-main.js
@@ -0,0 +1,9 @@
+function villanykortefelkapcsol(){
+    const csere = document.getElementById("csere");
+    csere.setAttribute("src", "pic_bulbon.gif");
+}
+
+function villanykortelekapcsol(){
+    const csere = document.getElementById("csere");
+    csere.setAttribute("src", "pic_bulboff.gif");
+}
