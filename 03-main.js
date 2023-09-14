949e4e3a14482a76391cff629aa1755dbdd89aed
Boza Patrik <tempestsama002@gmail.com>
2023-09-14 20:26:01 +0200

    02modifirtred

--- 03-main.js
+++ 03-main.js
@@ -7,3 +7,24 @@ function villanykortelekapcsol(){
     const csere = document.getElementById("csere");
     csere.setAttribute("src", "pic_bulboff.gif");
 }
+
+function bevitelFocus(){
+    console.log("Belekattintott a beviteli mezőbe!")
+}
+
+function bevitelBlur(){
+    const bevtel = document.getElementById("bevitel")
+    const beirtErtek = bevtel.value;
+    console.log("Mezőbe Írt érték:", beirtErtek);
+}
+
+function bevitelInput(){
+    const bekezdes = document.getElementById("uzenet");
+    bekezdes.innerHTML = "Nyisd meg a consolt! (ctrl+shift+i)";
+}
+
+function uzenetPiros(){
+    const uzenet = document.getElementById("uzenet")
+    uzenet.style.color = "red";
+    uzenet.style.backgroundColor ="#AAAAAA";
+}
